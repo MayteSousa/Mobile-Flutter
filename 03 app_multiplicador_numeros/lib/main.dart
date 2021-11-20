c8bdfb4cae831ee1e6233208e34e4c40386a14fa
Mayte Araujo <maypg@bol.com.br>
2021-11-20 00:53:21 -0300

    #App 03: Multiplicação de 2 números

---
+++ 03 app_multiplicador_numeros/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'Multiplicador.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Multiplicador(),
+    );
+  }
+}
+
+
+
+ 
